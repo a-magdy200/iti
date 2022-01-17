f48e8a70d56eeabe824ce8006e295f9a1653168c
Ahmed Magdy <ahmedmagdy.200m@gmail.com>
2022-01-17 13:19:29 +0200

    Added java lab 8 using css

--- java/lab8/helloCss/HelloWorldCSS.java
+++ java/lab8/helloCss/HelloWorldCSS.java
@@ -1,19 +1,14 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.effect.Reflection;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.LinearGradient;
-import javafx.scene.paint.Stop;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-public class HelloWorld extends Application {
-    public HelloWorld() {
+public class HelloWorldCSS extends Application {
+    public HelloWorldCSS() {
         System.out.println("Executing Constructor: " + Thread.currentThread().getName());
     }
     @Override
@@ -30,34 +25,25 @@ public class HelloWorld extends Application {
         // Declare Variables
         final int sceneWidth = 400;
         final int sceneHeight = 400;
-        // Create Text Component
+        // Create Components
         Text helloWorld = new Text("Hello World");
-        // Create Reflection Effect
-        Reflection reflection = new Reflection();
-        reflection.setFraction(0.8f);
-        // Add Effect to text component
-        helloWorld.setEffect(reflection);
-        // Set Font Color
-        helloWorld.setFill(Color.RED);
-        // Set Font Size
-        helloWorld.setFont(Font.font(60.0));
-        // Move text in Y direction
-        helloWorld.setTranslateY(-30);
-        // Create Gradient Color Stops
-        Stop[] stops = new Stop[] { new Stop(0, Color.BLACK), new Stop(0.5, Color.WHITE), new Stop(1, Color.BLACK)};
-        // Create Linear Gradient
-        LinearGradient linearGradient = new LinearGradient(0, 0, 0, 1, true, CycleMethod.NO_CYCLE, stops);
-        // Create rectangle to simulate background
+        Text reflectedText = new Text("Hello World");
         Rectangle rectangle = new Rectangle(0, 0, sceneWidth, sceneHeight);
-        // Apply gradient to rectangle
-        rectangle.setFill(linearGradient);
         // Create Root branch
         StackPane root = new StackPane();
+        // Create Scene
+        Scene scene = new Scene(root, sceneWidth, sceneHeight);
+        // Load css file
+        scene.getStylesheets().add(getClass().getResource("myStyles.css").toString());
+        // Apply styles to components
+        rectangle.getStyleClass().add("background");
+        helloWorld.getStyleClass().add("text");
+        reflectedText.getStyleClass().add("reflectedText");
         // Add Components to root branch
         root.getChildren().add(rectangle);
         root.getChildren().add(helloWorld);
-        // Create Scene
-        Scene scene = new Scene(root, sceneWidth, sceneHeight);
+        root.getChildren().add(reflectedText);
+
         // Add scene to stage
         primaryStage.setScene(scene);
         // Change app layout
