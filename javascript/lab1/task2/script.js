3c603d9c2f5ab7f0611a9b1e3121cfd180474e7f
Ahmed Magdy <ahmedmagdy.200m@gmail.com>
2022-01-23 13:30:30 +0200

    Updated javascript lab 1

--- javascript/lab1/task2/script.js
+++ javascript/lab1/task2/script.js
@@ -1,8 +1,7 @@
-
-let number1, result, number2, running = true, feedback, operation;
+let number1, result = undefined, number2, running = true, feedback, operation;
 const operations = ['sum', 'subtract', 'division', 'multiply', 'modulus'];
 while (running) {
-  number1 = result ? result : parseInt(prompt("Input first number"), 10);
+  number1 = result !== undefined ? result : parseInt(prompt("Input first number"), 10);
   number2 = parseInt(prompt("Input second number"), 10);
   if (isNaN(number1) || isNaN(number2)) {
     alert("one or more entries is not a valid number");
