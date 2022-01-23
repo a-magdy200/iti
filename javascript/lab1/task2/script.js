cf2a2711ffbd7b5f9128dca700370f72168e9fcb
Ahmed Magdy <ahmedmagdy.200m@gmail.com>
2022-01-23 13:02:32 +0200

    Updated javascript lab 1: Added multiple operations

--- javascript/lab1/task2/script.js
+++ javascript/lab1/task2/script.js
@@ -1,12 +1,34 @@
 
-let number1, result, number2, running = true, feedback;
+let number1, result, number2, running = true, feedback, operation;
+const operations = ['sum', 'subtract', 'division', 'multiply', 'modulus'];
 while (running) {
   number1 = result ? result : parseInt(prompt("Input first number"), 10);
   number2 = parseInt(prompt("Input second number"), 10);
   if (isNaN(number1) || isNaN(number2)) {
     alert("one or more entries is not a valid number");
+    continue;
   }
-  result = number1 + number2;
+  operation = prompt("Enter operation (" + operations.join(", ") + ")").toLowerCase();
+  switch (operation) {
+    case operations[0]:
+      result = number1 + number2;
+      break;
+    case operations[1]:
+      result = number1 - number2;
+      break;
+    case operations[2]:
+      result = number1 / number2;
+      break;
+    case operations[3]:
+      result = number1 * number2;
+      break;
+    case operations[4]:
+      result = number1 % number2;
+      break;
+    default:
+      alert("Invalid operation");
+      continue;
+  }
   alert("Result is: " + result);
   feedback = prompt("Continue? (yes\\NO)").toLowerCase();
   running = feedback === 'y' || feedback === 'yes';
