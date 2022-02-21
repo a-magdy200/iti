d125df44ba2506f1e2bb5d193d6c32e182b929d4
Ahmed Magdy <ahmedmagdy.200m@gmail.com>
2022-02-21 17:53:31 +0200

    Added php day 1

---
+++ php/day1/simple-form.php
@@ -0,0 +1,95 @@
+<?php
+$vars = [
+    'name' => '',
+    'email' => '',
+    'message' => ''
+];
+$errors = [];
+$success = false;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $vars['name'] = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    $vars['email'] = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $vars['message'] =  filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+
+    if (isset($vars['name']) && strlen($vars['name']) != 0) {
+        if (strlen($vars['name']) > 99) {
+            $errors[] = "Name is too long, must be less than 100 characters";
+        }
+    } else {
+        $errors[] = 'Name is required';
+    }
+    if (isset($vars['message']) && strlen($vars['message']) != 0) {
+        if (strlen($vars['message']) > 254) {
+            $errors[] = "Message is too long, must be less than 255 characters";
+        }
+    } else {
+        $errors[] = 'Message is required';
+    }
+    if (isset($vars['email']) && strlen($vars['email']) != 0) {
+        if (!filter_var($vars['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors[] = "Please input a valid email";
+        }
+    } else {
+        $errors[] = 'Email is required';
+    }
+    $success = count($errors) === 0;
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css" />
+</head>
+<body>
+    <div class="container">
+        <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+            <?php
+            if (count($errors) > 0) {
+                echo "<ul>";
+                foreach ($errors as $error) {
+                    echo "<li class='error'> $error </li>";
+                }
+                echo "</ul>";
+            }
+            ?>
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input value="<?php echo $vars['name']; ?>" type="text" id="name" name="name" placeholder="Name">
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input value="<?php echo $vars['email']; ?>" type="email" id="email" name="email" placeholder="Email@Domain.com">
+            </div>
+            <div class="form-group">
+                <label for="message">Message</label>
+                <textarea name="message" id="message" cols="30" rows="3" placeholder="Message"><?php echo $vars['message']; ?></textarea>
+            </div>
+            <div>
+                <button type="reset">Clear</button>
+                <button type="submit">Submit</button>
+            </div>
+
+            <div class="success">
+                <?php
+                if ($success) {
+                    echo "<hr />";
+                    echo "<h2>Thank you For Contacting Us";
+                    echo "<hr />";
+                    foreach ($vars as $key => $value) {
+                        echo "<div>";
+                        echo "<span>$key&nbsp;&nbsp;&nbsp;&nbsp;</span>";
+                        echo "<span>$value</span>";
+                        echo "</div>";
+                    }
+                }
+                ?>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
