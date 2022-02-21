fd6144f2a3b5bf85a7ac47e16f2c2660dd2f7df3
Ahmed Magdy <ahmedmagdy.200m@gmail.com>
2022-02-21 18:08:12 +0200

    update php day 1, Added references in config file

--- php/day1/config.php
+++ php/day1/config.php
@@ -1,3 +1,12 @@
 <?php
 const MAX_NAME_LENGTH = 99;
 const MAX_MESSAGE_LENGTH = 254;
+/*
+References:
+
+https://www.php.net/manual/en/language.constants.php
+https://www.php.net/manual/en/function.filter-var
+https://www.php.net/manual/en/control-structures.foreach.php
+https://www.php.net/manual/en/function.require-once.php
+
+*/
