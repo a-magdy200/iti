5d1bc11d0308ea8c5e349bbad35d1d3eda52e7a4
Ahmed Magdy <ahmedmagdy.200m@gmail.com>
2022-03-07 18:47:35 +0200

    Added php lab #4,5

--- php/day4/index.php
+++ php/day4/index.php
@@ -1,9 +1,30 @@
 <?php
-// Create connection
-$conn = new mysqli(__HOST__, __USER__, __PASSWORD__, __DB_NAME__);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+  require_once "vendor/autoload.php";
+  require_once "autoload.php";
+  $dbConnection = new DBConnect();
+  $currentPage = $_SERVER['PHP_SELF'];
+  if (isset($_GET['product'])) {
+    $item = $dbConnection->get_record_by_id(intval($_GET['product']));
+    echo "<table><tbody>";
+    foreach ($item as $key=>$value) {
+      if (strtolower($key) === 'photo') {
+        $imagePath = str_replace("index.php", "images/$value", $currentPage);
+        echo "<tr><td>$key</td><td><img height='100' src=\"$imagePath\"  alt=\"Product Image\"/></td></tr>";
+      } else {
+        echo "<tr><td>$key</td><td>$value</td></tr>";
+      }
+    }
+    echo "</tbody></table>";
+  } else {
+    $items = $dbConnection->get_data();
+    $currentPage = substr($currentPage, 0, strlen($currentPage) - 1);
+    echo "<table><thead>";
+    echo "<tr><th>ID</th><th>Name</th><th>Details</th></tr>";
+    echo "</thead>";
+    echo "<tbody>";
+    foreach ($items as $item) {
+      echo "<tr><td>$item->id</td><td>$item->product_name</td><td><a href=\"$currentPage?product=$item->id\">Details</a></td></tr>";
+    }
+    echo "</tbody></table>";
+    echo "<div><a href=\"". $currentPage . $items->previousPageUrl()."\">Prev</a><a href=\"". $currentPage . $items->nextPageUrl()."\">Next</a></div>";
+  }
--- php/day4/models/DbHandler.php
+++ php/day4/models/DbHandler.php
@@ -3,5 +3,5 @@ interface DbHandler {
     public function connect();
     public function get_data($fields = array(),  $start = 0);
     public function disconnect();   
-    public function get_record_by_id($id,$primary_key);
+    public function get_record_by_id($id);
 }
